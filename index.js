cbb06d23214e0676a2402d2f146954f72d1362c7
odera <eugeneodera59@gmail.com>
2024-10-06 20:08:42 +0300

    add text

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+function findMatching(drivers, searchString) {
+    const lowerCaseSearch = searchString.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase() === lowerCaseSearch);
+}
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+const matchingDrivers = findMatching(drivers, "Bobby");
+console.log(matchingDrivers); 
+
+function fuzzyMatch(drivers, query) {
+    const lowerCaseQuery = query.toLowerCase();
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowerCaseQuery));
+}
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+}
